850c165121d3ef50562515388a9fa85958d4c9e9
traluzzatheking90 <alessandro.malinverni@live.it>
2016-11-06 23:10:29 +0100

    Added genration random number and reset round method

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,14 +12,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
     
-    // the current value starts from 50 because is the center of the sider at startup
+    // the current value of the slider
     var currentValue: Int = 0
+    var targetValue: Int = 0
 
     override func viewDidLoad() {
-        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib. Good place to instance variables
         // Before loading Storyboard
-        currentValue = lroundf(slider.value)
+        super.viewDidLoad()
+        
+        self.startNewRound()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,7 +33,7 @@ class ViewController: UIViewController {
     @IBAction func showAlert(){
         // IB: inteface builder action, it is the show_alert controller.
         
-        let message = "The value of the slider is now: \(currentValue)"
+        let message = "The value of the slider is now: \(currentValue)" + "\n The target value is \(targetValue)"
         
         let alert = UIAlertController(title: "Slider Value",
                                       message: message,
@@ -43,11 +46,21 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        self.startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider:UISlider){
         //print("The value of the slider is now: \(slider.value)")
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound(){
+        // this function generate a random number between 0 and 99
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    
     }
 
 }
